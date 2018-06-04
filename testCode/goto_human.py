e50c65d76b020af513d75aba5251eeaedf92db9b
Garrett Wilson <g@floft.net>
2018-06-04 15:49:46 -0700

    Finished rotate RAS, untested

--- testCode/goto_human.py
+++ testCode/goto_human.py
@@ -22,6 +22,8 @@ class GoToHuman():
 
     global lastknown_human
     global lastknown_RAS
+    global lastknown_quat
+    lastknown_quat = 0.0
     lastknown_human = (2.0, 2.0)
     lastknown_RAS = (0.0, 0.0)
 
@@ -40,10 +42,11 @@ class GoToHuman():
         locations = self.getLocation()
         human = locations[0]
         RAS = locations[1]
+        quat = locations[2]
 
         if (human==RAS): print("Incorrect") #Shouldn't be same location, means it wasn't updated from listener
 
-        msg = self.doMath(human[0], human[1], RAS[0], RAS[1])       
+        msg = self.doMath(human[0], human[1], RAS[0], RAS[1], quat)       
 	
 
         #log stuff
@@ -56,13 +59,15 @@ class GoToHuman():
         return msg #message from moving RAS
 
 
-    def doMath(self, h_x, h_y, r_x, r_y):
+    def doMath(self, h_x, h_y, r_x, r_y, quat):
 
         #Do math, given location A and B, straight line between the two with 2ft gap between
 
         rospy.wait_for_service('goto_xy') #wait for service to start, need to start yourself (run file)
         goto_human = rospy.ServiceProxy('goto_xy', Goto_xy)
 
+        rospy.wait_for_service('rotate') #wait for service to start, need to start yourself (run file)
+        rotate_to_human = rospy.ServiceProxy('rorate', Rotate)
 
         if ((h_x == r_x) or (h_y == r_y)):
 
@@ -73,6 +78,7 @@ class GoToHuman():
             
                 #rosservice call goto_xy h_x, h_y-0.6096
                 horizontal = goto_human(h_x, h_y-0.6096)
+                rotate_to_human(quat)
                 return horizontal.response
             
             else:
@@ -80,6 +86,7 @@ class GoToHuman():
                 goto(h_x-0.6096, h_y) #vertical  match, change this to be more forgiving with in 5 degrees?
                 #rosservice call goto_xy h_x-0.6096, h_y
                 vert = goto_human(h_x-0.6096, h_y)
+                rotate_to_human(quat)
                 return triangle.response
 
         else:
@@ -88,24 +95,28 @@ class GoToHuman():
             # and the legs would be 0.431m (1.41ft) each.
             #rosservice call goto_xy h_x-0.431, h_y-0.431
             triangle = goto_human(h_x-0.431, h_y-0.431)
+            rotate_to_human(quat)
             return triangle.response
+        
 
     
     def getLocation(self):
-        return [lastknown_human, lastknown_RAS]
+        return [lastknown_human, lastknown_RAS, lastknown_quat]
 
 
     def setRAS(self): #get data from tf listener
         #wiki.ros.org/tf/TfUsingPython
         if self.tf.frameExists("/base_link") and self.tf.frameExists("/map"):
             t = self.tf.getLatestCommonTime("/base_link", "/map")
-            position, quaternion = self.tf.lookupTransform("/base_link", "/map", t) #position is tuple (x, y, z)
+            position, quaternion = self.tf.lookupTransform("/base_link", "/map", t) #position is tuple (x, y, z), quat (x, y, z, w)
+
             lastknown_RAS = (position[0], position[1])
+            lastknown_quat = quaternion[3] #rotation angle from original position
         
 
     def setHuman(data): #get data from Human listener, currently incorrect
         # lastknown_human = (data.x, data.y)
-        lastknown_human = (2.0, 2.0)
+        lastknown_human = (2.0, 2.0) #testing with human 2m away from origin 
     
 
     def shutdown(self): 
---
+++ testCode/rotate.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import *
+
+def rotate_robot(notinput):
+    #tell the action client that we want to spin a thread by default
+    move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    rospy.loginfo("wait for the action server to come up")
+    #allow up to 5 seconds for the action server to come up
+    move_base.wait_for_server(rospy.Duration(5))
+
+    #we'll send a goal to the robot to move 3 meters forward
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = 'map'
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.orientation.w = notinput.w #Rotate
+
+    #start moving
+    move_base.send_goal(goal)
+
+    #allow TurtleBot up to 60 seconds to complete task
+    success = move_base.wait_for_result(rospy.Duration(60)) 
+
+
+    if not success:
+        move_base.cancel_goal()
+        rospy.loginfo("The base failed to rotate for some reason")
+        return "Failure" 
+
+    else:
+        # We made it!
+        state = move_base.get_state()
+        if state == GoalStatus.SUCCEEDED:
+            rospy.loginfo("Hooray, the base rotated")
+        return "Success"
+        
+
+
+class RotateToObject():
+    def __init__(self):
+        rospy.init_node('nav_test_rotate', anonymous=False)
+    	s = rospy.Service('rotate', Rotate, rotate_robot)
+    	rospy.loginfo("Beginning rotate service")
+    	rospy.spin()
+
+    	#what to do if shut down (e.g. ctrl + C or failure)
+    	rospy.on_shutdown(self.shutdown)
+
+    
+
+    def shutdown(self):
+        rospy.loginfo("Editing Service")
+
+
+if __name__ == '__main__':
+    try:
+        RotateToObject()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Exception thrown")
+
