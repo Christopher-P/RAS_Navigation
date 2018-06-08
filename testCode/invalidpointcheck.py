b22c0244f29b526f42d35f4aec3724adae829dbd
Garrett Wilson <g@floft.net>
2018-06-08 15:16:07 -0700

    Finished hardcode testing of finding nearest point. Moving to try and RAS test.

--- testCode/invalidpointcheck.py
+++ testCode/invalidpointcheck.py
@@ -2,118 +2,130 @@
 
 
 '''
-The goal behind this code is the following:
-To subscribe to the ROS Topic for 'global/costmap'
-From that we will pull the raw information with a 2D array of points. 
-For the moment my small test set will have points from 0 to 2.
-After we get the data, we will need to figure out where RAS is inside that
-costmaps points. 
-
-When this service is called it will also need the proposed point RAS should move to. 
-We will check where that point is, if it is inside a collision/inflation point we 
-want to pick the next closest point.
-
-In this current version you don't need to know RAS' position. What you're looking for
-in this case is the closest free space from an point that isn't allowed to be moved in.
+This service will take a point given to use in the local map. 
+The map of the room will be processed into numpy grid. 
+Given the point we will find it in the numpy grid. 
+We will decide if it is an acceptable point below the threshold. 
+Once we find that point we will return it in right coordinates. 
+The coordiates we return are in the same format as the given ones.
 '''
 
 
-import rospy, math, time, sys, os, random
+import rospy, random
 import numpy as np
-# from tf import TransformListener
-# from ras_msgs.srv import Localize
 
-
-#Setup the map to use, I'll need to mess with this once I get actual costmap so
-#I can make sure numpy has it oriented properly, or preferably we can work with it directly
-#As given. Will still need to ensure the x,y is properly oriented with what I have here. 
+ 
 class NewMap():
 
 
-    #When we get costmap we will pass it here and just have it be the map, hopefully. 
-    def __init__(self):
+    #Given the text file of the map we find all the needed data.
+    def __init__(self, m_file, th):
 
-        with open("fakemap.txt") as textFile:
-            lines = [line.split() for line in textFile]
+        self.th, self.m_file = th, m_file
+        self.Map, self.origin, self.columns, self.rows, self.y_offset, self.x_offset = self.process(self.m_file)
 
-        self.Map = np.array(lines, dtype=np.int8)
-        self.Map = np.flipud(self.Map)
-        #This is to make the coordinates traditional bottom,left is 0,0
-        #However this needs to be double checked with an actual costmap to see what the orientation is.
+        
+    #Process the file to get the needed data. 
+    def process(self, m_file):
+       
+        with open("mapmap.txt") as map_file:
 
+            xy_flag = False
+           
+            for line in map_file:
+               
+                name_info = line.lstrip().rstrip().split(":")
+         
+                if name_info[0] == "width": cols = int(name_info[1]) #width of map
+                
+                elif name_info[0] == "height": rows = int(name_info[1]) #Height of map
 
-    #Delete soon, was function used to make fake maps
-    def changeIt(self, start, end, row, changeTo):
+                elif name_info[0] == "data": #Get all of the points in the map and format them in numpy
+                   
+                    raw_data = name_info[1].lstrip()[1:-1]
+                    raw_data_list = map(str.strip, raw_data.strip().split(','))
+                    pointer = 0
+                    np_map = np.zeros([rows, cols], dtype=int)
 
-        for i in range (start, end + 1):
-            self.Map[row][i] = changeTo
+                    for row in range(rows): 
+                        for col in range(cols):
+                         
+                            np_map[row, col] = int(raw_data_list[pointer])
+                            pointer += 1
 
+                elif name_info[0] == "position": xy_flag = True #Set flag to get correct xy, instead of the orientation xy.
 
-    #Remove when visuals not needed
-    def printMap(self):
+                elif xy_flag and name_info[0]=="x": xo = int(round(float(name_info[1]) * 20)) #set x offset
 
-        np.set_printoptions(threshold='nan')
-        print(self.Map)
-
+                elif xy_flag and name_info[0]=="y":
+                 
+                    yo = int(round(float(name_info[1]) * 20)) #set y offset
+                    xy_flag = False #Set so we don't overwrite the correct xy
+            
+        np_map = np.flipud(np_map) #Flip it to set it up properly for the math implimented
+        org = (-yo, -xo) #Set origin
+        return np_map, org, cols, rows, yo, xo
+   
 
     #Returns T/F for if the requested point is acceptable or not.
     def wantedPoint(self, point):
-        if self.Map[point[0]][point[1]] > 30: return False
-        else: return True
+      
+        return np.logical_and(self.getValue(point) >= 0, self.getValue(point) < self.th) #This is abnormal because numpy was having issues with the 0 < x < n being ambiguous
 
 
-    #Finds the closest free point to your requested point, using spiral pattern.
+    #Get the value at the requested point
+    def getValue(self, point):
+       
+        return self.Map[point]
+
+
+    #Finds the closest free point to your requested point, using spiral pattern. https://stackoverflow.com/questions/398299/looping-in-a-spiral
     def closestPoint(self, point):
-        #https://stackoverflow.com/questions/398299/looping-in-a-spiral
-        #Going to search for points in a spiral out from original point. 
- 
-        shape = self.Map.shape
-        print(shape) #Debug
-        row_count = shape[0]
-        col_count = shape[1]
+      
         row = col = 0
         d_row = 0
         d_col = -1
 
-        for i in range(max(row_count, col_count)**2):
-
+        for i in range(max(self.rows, self.columns)**2):
             if row == col or (row < 0 and row == -col) or (row > 0 and row == 1 - col):
+            
                 d_row, d_col = -d_col, d_row
 
             row, col = row + d_row, col + d_col
+            new_row, new_col = point[0] + row, point[1] + col 
+         
+            #If the points are inbounds and its the correct value for the th then return these new points.
+            if ((0 <= new_row < self.rows) and (0 <= new_col < self.columns)) and self.wantedPoint((new_row, new_col)): return (new_row, new_col)
 
-            new_row = point[0] + row 
-            new_col = point[1] + col
-            in_bounds = (0 <= new_row < row_count) and (0 <= new_col < col_count)
-            
-            if in_bounds and self.wantedPoint((new_row, new_col)):
-                self.Map[new_row, new_col] = 11 #to see ending point, remove when visuals not needed
-                return (new_row, new_col)
-            elif in_bounds:
-                self.Map[new_row][new_col] = 33 #To see path, remove when visuals not needed
-            
+    
+    #Convert from RAS to Numpy
+    def RAStoNumpy(self, point):
+       
+        return(point[1] - self.y_offset, point[0] - self.x_offset)
 
+
+    #Convert from Numpy to RAS
+    def NumpytoRAS(self, point):
+       
+        return (point[1] + self.x_offset, point[0] + self.y_offset)
+
+                      
 if __name__ == '__main__':
    
     try:
-
-        map = NewMap()            
-        point = (12, 00) #row, col not x,y with numpy
+   
+        map_file = "mapmap.txt" ### CHANGE WITH RAS DATA
+        threshold = 75
+        map = NewMap(map_file, threshold) 
+        point = (58, 19) ###CHANGE WITH RAS DATA
+        point = map.RAStoNumpy(point) 
         acceptable = map.wantedPoint(point)
-        map.Map[point] = 77 #To see start
 
-        while not acceptable: 
-            point = map.closestPoint(point)
-            acceptable = map.wantedPoint(point)
+        if not acceptable: point = map.closestPoint(point)
 
-        map.printMap() #Delete later, debug reasons now. 
+        point = map.NumpytoRAS(point) #Give back as something RAS can understand. 
+        print(point)        
 
+    except rospy.ROSInterruptException:
 
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-        print(e)
-
-    # except rospy.ROSInterruptException:
-    #     rospy.loginfo("Exception thrown")
+        rospy.loginfo("Exception thrown")
