032a06b01fc8b4b78a388ca6465fe8f838b61ab5
christopher.pereyda <christopher.pereyda@gmail.com>
2018-06-12 16:57:12 -0700

    Localization debugging

--- testCode/single_loc.py
+++ testCode/single_loc.py
@@ -10,93 +10,124 @@ for his current location. Going to try nested class.
 import rospy, math, time, sys, os
 from tf import TransformListener
 from ras_msgs.srv import Localize
+from std_msgs.msg import String
 
 
-class Localize:
-    def __init__(self):
-        rospy.init_node('nav_test_localize2', anonymous=False)
-        s = rospy.Service('localize', Localize, self.start_loc)
-        rospy.loginfo("Reporting localized service")
-        rospy.spin()
-        rospy.on_shutdown(rospy.loginfo("Ending Service"))
+class LocIt:
+	def __init__(self):
 
 
-    def start_loc(self, service_input):
-        print(service_input)
-        original = self.RAS()
-        starting_pos = original.pos
-        startimg_time = original.time
-        loc = self.Local(starting_pos[0], starting_pos[1], starting_time) #set to now
-        finished = False #are we localized?
+		rospy.init_node('nav_test_localize2', anonymous=False)
+		s = rospy.Service('localize', Localize, self.start_loc)
+		rospy.loginfo("Reporting localized service")
+		rospy.spin()
+		rospy.on_shutdown(rospy.loginfo("Ending Service"))
 
-        while not finished:
-            current_RAS = self.RAS()
-            current_pos = current_RAS.pos
-            current_time = current_RAS.time
-            loc.newLocal(current_pos[0], current_pos[1], current_time)
-            dis = loc.getDistance()
-            time_dif = loc.getTime()
-            scale = 1/time_dif
-            adjusted_dis = distance * scale
-            if adjusted_dis > 10:
-                rospy.loginfo("Localized, moving faster than 10m/s")
-                print("Localized")
-                finished = True
-            else: 
-                print("Not localized moving at", adjusted_dis, "m/s")
+	def start_loc(self, notdatainput):
 
 
-    def publishIt(self):
-        pub = rospy.Publisher('localized', std_msgs.msg.String, queue_size=10)
-        pub.publish(std_msgs.msg.String("True"))
+
+		# print(service_input)
+		original = self.RAS()
+		original.getRAS()
+		starting_pos = original.pos
+		starting_time = original.thyme
+		loc = self.Local(starting_pos[0], starting_pos[1], starting_time) #set to now
+		finished = False #are we localized?
+		counter = -1
+
+		while not finished:
+			counter += 1
+			current_RAS = self.RAS()
+			current_RAS.getRAS()
+			current_pos = current_RAS.pos
+			current_time = current_RAS.thyme
+			loc.newLocal(current_pos[0], current_pos[1], current_time)
+			dis = loc.getDistance()
+			time_dif = loc.getTime()
+			scale = 1/time_dif
+			adjusted_dis = dis * scale
+			time.sleep(2.5)
+			if adjusted_dis > 10:
+				rospy.loginfo("Localized, moving faster than 10m/s")
+				finished = True
+				rospy.loginfo("Hooray, we've localized")
+				self.publishIt("True")
+				return "Success"
+			#elif counter > 15:
+			#	rospy.loginfo("Not localized and past interation point")
+			#	self.publishIt("False")
+			#	return "Failure"
+			else: 
+				log_it = "Haven't localized yet, going again " + str(int(round(adjusted_dis))) + "m/s " + str(counter)			
+				
+				rospy.loginfo(log_it)
 
 
-    class Local:
-        def __init__(self, x, y, t):
-            self.current_local = self.last_local= (x, y)
-            self.current_time = self.last_time = t
+	def publishIt(self, mesg):
+		pub = rospy.Publisher('localized', String, queue_size=10)
+		pub.publish(mesg)
 
 
-        def newLocal(self, x, y, t):
-            self.last_local = self.current_local
-            self.current_local = (x, y)
-            self.last_time = self.current_time
-            self.current_time = t
+	class Local:
+		def __init__(self, x, y, t):
+			self.current_local = self.last_local= (x, y)
+			self.current_time = self.last_time = t
 
 
-        def getDistance(self):
-            x = self.current_local[0] - self.last_local[0]
-            y = self.current_local[1] - self.last_local[1]
-            return math.hypot(x, y)
+ 		def newLocal(self, x, y, t):
+			before = "Current local: " + str(self.current_local) + ". Current time: " + str(self.current_time)
+			print(before)
+			self.last_local = self.current_local
+			self.current_local = (x, y)
+			self.last_time = self.current_time
+			self.current_time = t
+			after = "Updated local: " + str(self.current_local) + ". Updated time: " + str(self.current_time)
+			print(after)
+
+		def getDistance(self):
+			x = self.current_local[0] - self.last_local[0]
+			y = self.current_local[1] - self.last_local[1]
+			return math.hypot(x, y)
         
 
-        def getTime(self):
-            if(self.current_time > self.last_time): #Should always be this way
-                return self.current_time - self.last_time
+		def getTime(self):
+			if(self.current_time > self.last_time): #Should always be this way
+ 				return self.current_time - self.last_time
 
-            else:
-                rospy.loginfo("Unexpected Time")
-                return abs(self.current_time - self.last_time)
-
-
-    class RAS:
-        def __init__(self):
-            self.tf = TransformListener()
-            self.pos = self.getRAS()
-            self.time = time.time()
+			else:
+				rospy.loginfo("Unexpected Time")
+				return abs(self.current_time - self.last_time)
 
 
-        def getRAS(self):
-            if self.tf.frameExists("/base_link") and self.tf.frameExists("/map"):
-                t = self.tf.getLatestCommonTime("/base_link", "/map")
-                pos, quat = self.tf.lookupTransform("/base_link", "/map", t)
-                return(pos[0], pos[1]) # (x,y)
+	class RAS:
+ 		def __init__(self):
+			self.pos = None
+			self.thyme = None
 
 
+		def getRAS(self):
+
+			tf = TransformListener()
+			tf.waitForTransform("/base_link", "/map", rospy.Time(), rospy.Duration(1.0))			
+			finding = True
+			count = 0
+			while finding:
+				count += 1
+				tf.waitForTransform("/base_link", "/map", rospy.Time(), rospy.Duration(1.0))
+				(trans, rot)=tf.lookupTransform("/base_link", "/map", rospy.Time())
+				self.pos = (trans[0], trans[1])
+				self.thyme = time.time()
+				if self.pos is not None:
+					finding = False
+
+
+			
 
 
 if __name__ == '__main__':
-    try:
-        Localize()
-    except rospy.ROSInterruptException:
-        rospy.loginfo("Exception thrown")
+	try:
+
+		LocIt()
+	except rospy.ROSInterruptException:
+		rospy.loginfo("Exception thrown")
